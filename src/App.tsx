d25174e52e759df6007679a73852055997e9ac53
Pouya <puya_r@felixin.io>
2024-04-02 15:06:11 +0330

    feat(pricing):create pricing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
 
 import PageNotFound from "./pages/PageNotFound";
 import Homepage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/product" element={<Product />} />
-        <Route path="./pricing" element={<Pricing />} />
+        <Route path="/pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
